356bfa2ff47a035adc174b0adb38f31cf4fc69a8
mariazona122 <mariazona122@gmail.com>
2020-02-20 14:13:11 -0300

    Code Igniter Tutorial

---
+++ projeto_01/application/controllers/Helloworld.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Helloworld extends CI_Controller
+{
+        public function __construct()
+        {
+            parent::__construct();
+            //load model
+            $this->load->model('helloworld_model');
+        }
+
+        public function index()
+        {
+            //get data from the database
+            $data['result'] = $this->helloworld_model->getData();
+            //load view and pass the data
+            $this->load->view('helloworld_view', $data);
+
+        }
+}
